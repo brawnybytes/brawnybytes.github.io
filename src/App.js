e72a5da5bbf3988069eaa9bdbbe3b99d04c9d671
Manjunathsr355 <manjunathsr355@gmail.com>
2024-04-17 21:38:33 +0530

    minor css changes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,46 @@ import './App.css';
 import './App.scss';
 import contact from './resources/img/contact.jpg';
 import services from './resources/img/services.jpg';
+import logo from './resources/img/logo_close_up.png';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
 
 const App = () => {
+
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   useEffect(() => {
     let sections = document.querySelectorAll(".section"),
       images = document.querySelectorAll(".background"),
@@ -86,16 +124,122 @@ const App = () => {
 
   return (
     <div className="app-container">
-      <header className="header">
-        <nav>
-          <a href="#first">Home</a>&nbsp;&nbsp;
-          <a href="#second">Services</a>&nbsp;&nbsp;
-          <a href="#fifth">Contact</a>
-        </nav>
-      </header>
-      <Section id="first" title="Nova Enigma" className="first" bgUrl="https://images.unsplash.com/photo-1605629713998-167cdc70afa2?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2OTg2OTM1NTR8&ixlib=rb-4.0.3&q=85" />
-      <Section id="second" title="Services" className="second" bgUrl={services} />
-      <Section id="fifth" title="Contact" className="fifth" bgUrl={contact} />
+       <AppBar position="static">
+    <Container maxWidth="xl" className="container">
+      <Toolbar disableGutters>
+      <Toolbar className="tool">
+          <Typography variant="title">
+            <img src={logo}  height={80}  />
+          </Typography>
+        </Toolbar>
+       
+       
+
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: 'block', md: 'none' },
+            }}
+          >
+            {pages.map((page) => (
+              <MenuItem key={page} onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">{page}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+        <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+        <Typography
+          variant="h5"
+          noWrap
+          component="a"
+          href="#app-bar-with-responsive-menu"
+          sx={{
+            mr: 2,
+            display: { xs: 'flex', md: 'none' },
+            flexGrow: 1,
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+            color: 'inherit',
+            textDecoration: 'none',
+          }}
+        >
+          LOGO
+        </Typography>
+        <div>
+        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+         
+              <header className="header" >
+                <nav className="navHeader">
+                  <a href="#first">Home</a>&nbsp;&nbsp;
+                  <a href="#second">Services</a>&nbsp;&nbsp;
+                  <a href="#fifth">Contact</a>
+                </nav>
+              </header>
+              <Section id="first" title="Nova Enigma" className="first" bgUrl="https://images.unsplash.com/photo-1605629713998-167cdc70afa2?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2OTg2OTM1NTR8&ixlib=rb-4.0.3&q=85" />
+              <Section id="second" title="Services" className="second" bgUrl={services} />
+              <Section id="fifth" title="Contact" className="fifth" bgUrl={contact} />
+        </Box>
+        </div>
+       
+        
+
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: '45px' }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <Typography textAlign="center">{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+      </Toolbar>
+    </Container>
+  </AppBar>
     </div>
   );
 };
@@ -110,6 +254,7 @@ const Section = ({ id, title, className, bgUrl }) => {
         </div>
       </div>
     </section>
+   
   );
 };
 
